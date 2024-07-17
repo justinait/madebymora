4b4381aed2de23aad3f0affb87372be699983d81
justinait <justiiturraspe@gmail.com>
2024-07-17 13:46:18 -0300

    data json

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
 
 function Home() {
+  
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+      
+    fetch('/data.json')
+    .then((response) => response.json())
+    .then((responseData) => {
+        
+      setProjects(responseData.projects);
+  
+    });
+  }, []);
   return (
     <div>
-        <p>
-            These are some things that I have been developing during student and profesional career, 
-            you can find from graphic design, 3d modeling to immersive audiovisual rooms.
-        </p>
+      <p>About</p>
 
-        <p>About</p>
+      <div>
+        <h1 className='titleMora'>madebymora ©projects</h1>
+        <p className='subtitleMora'>Integral & Multidisciplinary Designer</p>
+      </div>
 
-        <div>
-            <h1 className='titleMora'>madebymora</h1>
-            <p className='subtitleMora'>Integral & Multidisciplinary Designer</p>
-        </div>
+      <div className='categoryBox'>
+        <p className='categoryItem'>All</p>
+        <p className='categoryItem'>Graphic</p>
+        <p className='categoryItem'>Audiovisual</p>
+        <p className='categoryItem'>UX&UI</p>
+        <p className='categoryItem'>3D</p>
+      </div>
 
-        <div className='categoryBox'>
-            <p className='categoryItem'>All</p>
-            <p className='categoryItem'>Graphic</p>
-            <p className='categoryItem'>Audiovisual</p>
-            <p className='categoryItem'>UX&UI</p>
-            <p className='categoryItem'>3D</p>
-        </div>
+      <div className='allProjectsBox'>
+        {
+        projects.map(e=> {
+          return (
+            <div>
+              <p className='projectTitle'>{e.name}</p>
+              <p className='projectDetail'>{e.detail}</p>
+              <p className='projectDescription'>{e.description}</p>
+              
+            </div>  
+          )
+        })
+        }
+      </div>
     </div>
   )
 }
