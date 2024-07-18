2025546aafc67d330b46c110cfe5d8467a2982ef
justinait <justiiturraspe@gmail.com>
2024-07-18 19:26:28 -0300

    navbar

--- src/Components/HeroProjects/HeroProjects.jsx
+++ src/Components/HeroProjects/HeroProjects.jsx
@@ -31,34 +31,34 @@ function HeroProjects() {
     <div>
         
 
-        <div className='titlesBoxHero'>
-            <h1 className='titleMora'>madebymora ©projects</h1>
-            <p className='subtitleMora'>Integral & Multidisciplinary Designer</p>
-        </div>
+      <div className='titlesBoxHero'>
+        <h1 className='titleMora'>madebymora ©projects</h1>
+        <p className='subtitleMora'>Integral & Multidisciplinary Designer</p>
+      </div>
 
-        <div className='heroProjectsBox'>
-            {
-            projects.map((e, index)=> {
-                return (
-                    <div className={`image-wrapper ${getClassName(index)}`}>
-                        <img src={e.image} alt={e.name} className='heroProjectsImage' />
-                    </div>  
-                )
-            })
-            }
-        </div>
+      <div className='heroProjectsBox'>
+        {
+        projects.map((e, index)=> {
+          return (
+            <div className={`image-wrapper ${getClassName(index)}`}>
+              <img src={e.image} alt={e.name} className='heroProjectsImage' />
+            </div>  
+          )
+        })
+        }
+      </div>
 
-        <div>
-            <Link to='about'>About</Link>
+      <div className='optionsHeroProjects'>
+        <Link to='about' className='aboutLink' >About</Link>
 
-            <div className='categoryBox'>
-                <p className='categoryItem'>All</p>
-                <p className='categoryItem'>Graphic</p>
-                <p className='categoryItem'>Audiovisual</p>
-                <p className='categoryItem'>UX&UI</p>
-                <p className='categoryItem'>3D</p>
-            </div>
+        <div className='categoryBox'>
+          <p className='categoryItem'>All</p>
+          <p className='categoryItem'>Graphic</p>
+          <p className='categoryItem'>Audiovisual</p>
+          <p className='categoryItem'>UX&UI</p>
+          <p className='categoryItem'>3D</p>
         </div>
+      </div>
     </div>
   )
 }
