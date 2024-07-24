ba7bdc751bd9da48183e3dab59aadf33a34bd94d
justinait <justiiturraspe@gmail.com>
2024-07-24 11:13:01 -0300

    about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,49 +7,56 @@ function About() {
     <div className='aboutContainer'>
       <div className='titlesContainerAbout'>
         <Link to='/home'><h2 className='titleMora'>madebymora ©projects</h2></Link>
-        
         <p className='subtitleMora'>Integral & Multidisciplinary Designer</p>
       </div>
 
       <p className='cvAbout'>
+        <strong>
         Mora Ramírez Amable
+        </strong>
         <br /> <br />
         As an integral designer, I have cultivated a global perspective on design, ensuring that every project I engage in maintains coherence and value across all its areas. I am ambitious and proactive, always eager to propose innovative and unrestricted ideas to maximize creativity before finding the best solution. This approach has allowed me to generate significant and original results in every challenge I have faced. 
         In addition to my technical skills, I consider myself a highly motivated person committed to excellence. I am convinced that my ability to work in a team, along with my user-centered approach and attention to detail, will positively contribute to every project I undertake.
       </p>
-      <p className='cvAbout skills'>
-        Skills 
-        <br /> <br />
-        Adobe
-        <br />
-        Photoshop / Ilustrator / After Effects / Indesign / Figma
 
-        <br /><br />
-        Illustration
-        <br />
-        Procreate
-        <br /><br />
-        3D
-        <br />
-        Sharp3d / Rhinoceros / Blender / Enscape
-        <br /><br />
-        Sound <br />
-        Reaper
-        
-        <br /><br />
-        Visual <br />
-        Touchdesigner <br /> Resolume Arena
-      </p>
+      <div className='skillsAboutDiv'>
+        <h6>        Skills       </h6>
+        <p className='cvAbout skills'>
+          Adobe
+          <br />
+          Photoshop / Ilustrator / After Effects / Indesign / Figma
 
-      <div className='linksAbout'>
-        <a target='_blank' href="mailto:moraramirezamable@gmail.com">Email</a>
-        <a target='_blank' href='https://www.instagram.com/madebymor4/' >Instagram</a>
-        <a target='_blank' href='https://www.linkedin.com/in/moraramirezamable/'  >LinkedIn</a>
-        <a target='_blank' href='https://www.behance.net/moraramirez'>Behance</a>
-        <br />
-        <a target='_blank' href='https://drive.google.com/file/d/1mYiC545i_zGN7fjyR0corRf-kpgFRJ8t/view?usp=sharing'>Download Vitae</a>
-        
+          <br /><br />
+          Illustration
+          <br />
+          Procreate
+          <br /><br />
+          3D
+          <br />
+          Sharp3d / Rhinoceros / Blender / Enscape
+          <br /><br />
+          Sound <br />
+          Reaper
+          
+          <br /><br />
+          Visual <br />
+          Touchdesigner <br /> Resolume Arena
+        </p>
       </div>
+
+      <div className='skillsAboutDiv contact'>
+        <h6>        Contact       </h6>
+        <div  className='linksAbout'>
+
+          <a target='_blank' href="mailto:moraramirezamable@gmail.com">Email</a>
+          <a target='_blank' href='https://www.instagram.com/madebymor4/' >Instagram</a>
+          <a target='_blank' href='https://www.linkedin.com/in/moraramirezamable/'  >LinkedIn</a>
+          <a target='_blank' href='https://www.behance.net/moraramirez'>Behance</a>
+          <a target='_blank' href='https://drive.google.com/file/d/1mYiC545i_zGN7fjyR0corRf-kpgFRJ8t/view?usp=sharing'>Download Vitae</a>
+        </div>
+      </div>
+
+      <img className='cvAboutImg' src="" alt="" />
     </div>
   )
 }
