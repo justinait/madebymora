4704935814612ace38d74be863e6eea53e34e31a
justinait <justiiturraspe@gmail.com>
2024-07-30 11:25:07 -0300

    replace carrusel

--- src/Components/HeroProjects/HeroProjects.jsx
+++ src/Components/HeroProjects/HeroProjects.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './HeroProjects.css'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Contact from '../Contact/Contact';
 
 function HeroProjects({ onCategoryChange }) {
@@ -12,12 +9,84 @@ function HeroProjects({ onCategoryChange }) {
   const [isBlurred, setIsBlurred] = useState(false)
   const [selectedCategory, setSelectedCategory] = useState('All');
   const categories = [     'All' , 'Graphic', 'Audiovisual', 'Photos', '3D']
-
+  const containerRef = useRef(null);
   const extraProjects = [
     {   name: "CHOCOLATE EN RAMA", image: "/images/photography/photo1.jpg", detail2: "PHOTOGRAPHY"   },
     {   name: "REVOLUTION 909", image: "/images/photography/photo7.jpg", detail2: "PHOTOGRAPHY"       }
   ]
-  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const slides = document.querySelectorAll('.heroProjectSlide');
+      if (slides.length > 0) {
+        setCurrentIndex((prevIndex) => {
+          const newIndex = (prevIndex + 1) % slides.length;
+          updateSlideClasses(newIndex);
+          return newIndex;
+        });
+      }
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [projects.length]);
+
+  const updateSlideClasses = (newIndex) => {
+    const slides = document.querySelectorAll('.heroProjectSlide');
+    slides.forEach((slide, index) => {
+      slide.classList.remove('prev', 'next', 'active');
+      if (index === newIndex) {
+        slide.classList.add('active');
+      } else if (index === (newIndex - 1 + slides.length) % slides.length) {
+        slide.classList.add('prev');
+      } else if (index === (newIndex + 1) % slides.length) {
+        slide.classList.add('next');
+      }
+    });
+
+    const container = containerRef.current;
+    if (container) {
+      const slideWidth = slides[0].clientWidth;
+      container.style.transform = `translateX(-${slideWidth * newIndex}px)`;
+    }
+  };
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     const container = document.querySelector('.projectsScrollable');
+  //     const slides = document.querySelectorAll('.heroProjectSlide');
+  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
+  //     if (container && slides.length > 0) {
+  //       const slideWidth = slides[0].clientWidth;
+  //       const currentTransform = container.style.transform || 'translateX(0%)';
+  //       const newTransform = currentTransform === 'translateX(0%)' ? `translateX(-${slideWidth}px)` : 'translateX(0%)';
+  //       container.style.transform = newTransform;
+
+
+  //       if (currentTransform === 'translateX(0%)') {
+  //         newTransform = `translateX(-${slideWidth}px)`;
+  //         newIndex = 1;
+  //       } else {
+  //         newTransform = 'translateX(0%)';
+  //         newIndex = 0;
+  //       }
+
+  //       container.style.transform = newTransform;
+
+  //       slides.forEach((slide, index) => {
+  //         slide.classList.remove('prev', 'next', 'active');
+  //         if (index === newIndex) {
+  //           slide.classList.add('active');
+  //         } else if (index === (newIndex - 1 + slides.length) % slides.length) {
+  //           slide.classList.add('prev');
+  //         } else if (index === (newIndex + 1) % slides.length) {
+  //           slide.classList.add('next');
+  //         }
+  //       });
+  //     }
+  //   }, 3000); // Cambia la slide cada 3 segundos
+
+  //   return () => clearInterval(intervalId); // Limpia el intervalo cuando el componente se desmonte
+  // }, [projects.length]);
+
   useEffect(() => {
     fetch('/data.json')
     .then((response) => response.json())
@@ -69,44 +138,6 @@ function HeroProjects({ onCategoryChange }) {
       setIsBlurred(false);
     }
   };
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    // centerPadding: '50px',
-    centerMode: true,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 4,
-          centerPadding: '40px',
-        }
-      },
-      {
-        breakpoint: 1300,
-        settings: {
-          centerPadding: '60px',
-        }
-      },
-      {
-        breakpoint: 1150,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          centerMode: true,
-          // centerPadding: '40px',
-        }
-      }
-    ]
-  };
 
   return (
     <div>
@@ -119,41 +150,41 @@ function HeroProjects({ onCategoryChange }) {
       </div>
 
       <div className='heroProjectsContainer'>
-
-        <Slider {...settings}>
-          {projects
-            .filter(e => e.name !== 'PHOTOGRAPHY')
-            .filter((e) => selectedCategory === 'All' || (e.brand && e.brand.some(brand => brand === selectedCategory)))
-            .map((project, index) => (
-              <div key={index} className="heroProjectSlide">
-                <div className="heroProjectInfo">
-                  <p>"{project.name}"</p>
-                  <p>{project.detail2}</p>
-                </div>
-                <div className="slider-fade-container">
-                  <img src={project.image} alt={project.name} className="heroProjectsImage" />
-                </div>
+      <div className='projectsScrollable'>
+        {projects
+          .filter(e => e.name !== 'PHOTOGRAPHY')
+          .filter((e) => selectedCategory === 'All' || (e.brand && e.brand.some(brand => brand === selectedCategory)))
+          .map((project, index) => (
+            <div 
+              key={index} 
+              className={`heroProjectSlide ${index === currentIndex ? 'active' : index === currentIndex - 1 ? 'prev' : index === currentIndex + 1 ? 'next' : ''}`}
+              // className="heroProjectSlide"
+              >
+              <div className="heroProjectInfo">
+                <p>"{project.name}"</p>
+                <p>{project.detail2}</p>
               </div>
+              <div className="slider-fade-container">
+                <img src={project.image} alt={project.name} className="heroProjectsImage" />
+              </div>
+            </div>
+        ))}
+        {extraProjects
+          .filter(e => selectedCategory === 'All' || selectedCategory === 'Photos' || (e.brand && e.brand.some(brand => brand === selectedCategory)))
+          .map((e, index)=> (
+            <div key={index} className="heroProjectSlide">
+              <div className="heroProjectInfo">
+                <p>"{e.name}"</p>
+                <p>{e.detail2}</p>
+              </div>
+              <div className="slider-fade-container">
+                <img src={e.image} alt={e.name} className="heroProjectsImage" />
+              </div>
+            </div>
           ))}
-          {
-            extraProjects
-              .filter(e => selectedCategory === 'All' || selectedCategory === 'Photos' || (e.brand && e.brand.some(brand => brand === selectedCategory))) // Segundo filtro
-              .map((e, index)=> {
-              return (
-                <div key={index} className="heroProjectSlide">
-                <div className="heroProjectInfo">
-                  <p>"{e.name}"</p>
-                  <p>{e.detail2}</p>
-                </div>
-                <div className="slider-fade-container">
-                  <img src={e.image} alt={e.name} className="heroProjectsImage" />
-                </div>
-              </div>
-              )
-            })
-          }
-        </Slider>
       </div>
+    </div>
+
 
       <div className='optionsHeroProjects'>
         <div className='heroLinks'>
