65ae877147d60b86a190f8559ff5978dbd4384c0
justinait <justiiturraspe@gmail.com>
2024-10-10 16:25:12 -0300

    million out

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import MillionLint from '@million/lint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    MillionLint.vite(), // Integración de Million.js para mejorar el rendimiento
-    react(),            // Plugin de React para soporte JSX, transformación de archivos y Fast Refresh
-  ],
-});
+  plugins: [react()],
+})
